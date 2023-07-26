0f40be3d4b697e7a72e9155023fba7af762e58a9
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-07-26 18:17:47 +0200

    added mutation

--- server/resolvers/post.js
+++ server/resolvers/post.js
@@ -1,14 +1,30 @@
 const { gql } = require("apollo-server-express");
-const {posts } = require("../temp")
+const { posts } = require("../temp");
 
 // const totalPosts = () => 42;
 
+// queries
 const totalPosts = () => posts.length;
 const allPosts = () => posts;
+
+// mutation
+const newPost = (parent, args) => {
+  console.log(args);
+  const post = {
+    id: posts.length + 1,
+    title: args.title,
+    description: args.description,
+  };
+  posts.push(post);
+  return post;
+};
 
 module.exports = {
   Query: {
     totalPosts,
-    allPosts
-  }
+    allPosts,
+  },
+  Mutation: {
+    newPost
+  },
 };
