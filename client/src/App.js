04316e92cdb27962d3d30c70834140ce25aead21
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-07-28 14:10:10 +0200

    set state with graphql data

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
-function App() {
-  return (
-    <p>Hello</p>
-  );
-}
+import React, { useState } from "react";
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+});
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  client.query({
+      query: gql`
+        {
+          allPosts {
+            id
+            title
+            description
+          }
+        }
+      `
+    })
+    .then(result => setPosts(result.data.allPosts));
+
+  return <p>Hello</p>;
+};
 
 export default App;
